236bfaba3fd2d6fc9cb64cd3a8a54fb15e43f0a2
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-09-02 15:45:31 +0300

    todo repository added

---
+++ persistence/todo_repository.go
@@ -0,0 +1,152 @@
+package persistence
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/pkg/errors"
+	"time"
+	"todo-app--go-gin/domain"
+)
+
+type ITodoRepository interface {
+	GetAllTodos() ([]domain.Todo, error)
+	GetTodoById(todoId int) (domain.Todo, error)
+	GetAllTodosByUserId(userId int) ([]domain.Todo, error)
+	AddTodo(todo domain.Todo) (domain.Todo, error)
+	UpdateTodo(todoId int, todo domain.Todo) (domain.Todo, error)
+	DeleteTodo(todoId int) error
+}
+
+type TodoRepository struct {
+	dbPool *pgxpool.Pool
+}
+
+func NewTodoRepository(dbPool *pgxpool.Pool) ITodoRepository {
+	return &TodoRepository{dbPool: dbPool}
+}
+
+func (todoRepository *TodoRepository) GetAllTodos() ([]domain.Todo, error) {
+	ctx := context.Background()
+	queryRow, err := todoRepository.dbPool.Query(ctx, "SELECT * FROM todos")
+	if err != nil {
+		return []domain.Todo{}, err
+	}
+
+	return extractTodosFromRows(queryRow), nil
+}
+
+func (todoRepository *TodoRepository) GetTodoById(todoId int) (domain.Todo, error) {
+	ctx := context.Background()
+	getByIdSql := `SELECT * FROM todos WHERE id = $1`
+	queryRow := todoRepository.dbPool.QueryRow(ctx, getByIdSql, todoId)
+
+	var id int
+	var userId int
+	var title string
+	var description string
+	var isCompleted bool
+	var createdAt time.Time
+	var updatedAt time.Time
+
+	scanErr := queryRow.Scan(&id, &userId, &title, &description, &isCompleted, &createdAt, &updatedAt)
+	if scanErr != nil {
+		if scanErr == sql.ErrNoRows {
+			return domain.Todo{}, fmt.Errorf("Todo not found with id %d", todoId)
+		}
+		return domain.Todo{}, fmt.Errorf("Error while getting todo with id %d: %v", todoId, scanErr)
+	}
+
+	return domain.Todo{
+		Id:          id,
+		UserId:      userId,
+		Title:       title,
+		Description: description,
+		IsCompleted: isCompleted,
+		CreatedAt:   createdAt,
+		UpdatedAt:   updatedAt,
+	}, nil
+}
+
+func (todoRepository *TodoRepository) GetAllTodosByUserId(userId int) ([]domain.Todo, error) {
+	ctx := context.Background()
+	getByIdSql := `SELECT * FROM todos WHERE user_id = $1`
+	queryRow, err := todoRepository.dbPool.Query(ctx, getByIdSql, userId)
+	if err != nil {
+		return []domain.Todo{}, err
+	}
+
+	return extractTodosFromRows(queryRow), nil
+}
+
+func (todoRepository *TodoRepository) AddTodo(todo domain.Todo) (domain.Todo, error) {
+	ctx := context.Background()
+	insertSql := `INSERT INTO todos (user_id, title, description, is_completed, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := todoRepository.dbPool.Exec(ctx, insertSql, todo.UserId, todo.Title, todo.Description, todo.IsCompleted, todo.CreatedAt, todo.UpdatedAt)
+	if err != nil {
+		return domain.Todo{}, err
+	}
+
+	return todo, nil
+}
+
+func (todoRepository *TodoRepository) UpdateTodo(todoId int, todo domain.Todo) (domain.Todo, error) {
+	ctx := context.Background()
+	updateSQL := `UPDATE todos SET user_id = $1, title = $2, description = $3, is_completed = $4, updated_at = $5 WHERE id = $6`
+	result, err := todoRepository.dbPool.Exec(ctx, updateSQL, todo.UserId, todo.Title, todo.Description, todo.IsCompleted, todo.UpdatedAt, todoId)
+	if err != nil {
+		return domain.Todo{}, err
+	}
+
+	rowsAffected := result.RowsAffected()
+	if rowsAffected == 0 {
+		return domain.Todo{}, fmt.Errorf("Todo with id %d not found", todo.Id)
+	}
+
+	return todo, nil
+}
+
+func (todoRepository *TodoRepository) DeleteTodo(todoId int) error {
+	ctx := context.Background()
+	_, getErr := todoRepository.GetTodoById(todoId)
+	if getErr != nil {
+		return errors.New("Product not found")
+	}
+
+	deleteSql := `DELETE FROM todos WHERE id = $1`
+	_, err := todoRepository.dbPool.Exec(ctx, deleteSql, todoId)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error while deleting product with id %d", todoId))
+	}
+
+	return nil
+}
+
+func extractTodosFromRows(queryRow pgx.Rows) []domain.Todo {
+	var todos = []domain.Todo{}
+	for queryRow.Next() {
+		var todo domain.Todo
+		err := queryRow.Scan(
+			&todo.Id,
+			&todo.UserId,
+			&todo.Title,
+			&todo.Description,
+			&todo.IsCompleted,
+			&todo.CreatedAt,
+			&todo.UpdatedAt,
+		)
+		if err != nil {
+			continue
+		}
+
+		todos = append(todos, todo)
+	}
+
+	if err := queryRow.Err(); err != nil {
+		return todos
+	}
+
+	return todos
+}
